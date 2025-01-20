723b99f044f9d5279cc740ab288c3e33f9dc4242
David Barreto <david.brto@gmail.com>
2025-01-20 02:12:32 +0000

    Solve typo

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -58,13 +58,13 @@ mod tests {
     fn read_u16() {
         // Given
         let address: u16 = 0x8000;
-        let less_significant_byte: u8 = 0x69;
+        let least_significant_byte: u8 = 0x69;
         let most_significant_byte: u8 = 0x96;
         let expected_data: u16 = 0x9669;
 
         // When
         let mut memory = Memory::new();
-        memory.write(less_significant_byte, address);
+        memory.write(least_significant_byte, address);
         memory.write(most_significant_byte, address+1);
 
         // Then
