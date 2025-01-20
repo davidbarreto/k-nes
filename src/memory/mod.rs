6e5a9dfc6080c3436ba0b4eb4da26f89f4001147
David Barreto <david.brto@gmail.com>
2025-01-20 16:51:37 +0000

    Move Memory unit tests to a separate file

--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -37,37 +37,4 @@ impl Memory {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn read_write() {
-        // Given
-        let address: u16 = 0x8000;
-        let expected_data: u8 = 0x69;
-
-        // When
-        let mut memory = Memory::new();
-        memory.write(expected_data, address);
-
-        // Then
-        assert_eq!(expected_data, memory.read(address));
-    }
-
-    #[test]
-    fn read_u16() {
-        // Given
-        let address: u16 = 0x8000;
-        let least_significant_byte: u8 = 0x69;
-        let most_significant_byte: u8 = 0x96;
-        let expected_data: u16 = 0x9669;
-
-        // When
-        let mut memory = Memory::new();
-        memory.write(least_significant_byte, address);
-        memory.write(most_significant_byte, address+1);
-
-        // Then
-        assert_eq!(expected_data, memory.read_u16(address));
-    }
-}
+mod tests;
