26baa1f7200ab134b7de5801698650473fc73e79
David Barreto <david.brto@gmail.com>
2025-01-21 01:31:02 +0000

    Add constants for the opcodes

--- src/cpu/opcode.rs
+++ src/cpu/opcode.rs
@@ -4,8 +4,30 @@ pub const ADC_IMMEDIATE: u8 = 0x69;
 pub const ADC_ZERO_PAGE: u8 = 0x65;
 pub const ADC_ZERO_PAGE_X: u8 = 0x75;
 pub const ADC_ABSOLUTE: u8 = 0x6D;
+pub const ADC_ABSOLUTE_X: u8 = 0x7D;
+pub const ADC_ABSOLUTE_Y: u8 = 0x79;
+pub const ADC_INDIRECT_X: u8 = 0x61;
+pub const ADC_INDIRECT_Y: u8 = 0x71;
 
-/// Opcodes for instruction set for 6402 processor
+pub const AND_IMMEDIATE: u8 = 0x29;
+pub const AND_ZERO_PAGE: u8 = 0x25;
+pub const AND_ZERO_PAGE_X: u8 = 0x35;
+pub const AND_ABSOLUTE: u8 = 0x2D;
+pub const AND_ABSOLUTE_X: u8 = 0x3D;
+pub const AND_ABSOLUTE_Y: u8 = 0x39;
+pub const AND_INDIRECT_X: u8 = 0x21;
+pub const AND_INDIRECT_Y: u8 = 0x31;
+
+pub const BPL: u8 = 0x10;
+pub const BMI: u8 = 0x30;
+pub const BVC: u8 = 0x50;
+pub const BVS: u8 = 0x70;
+pub const BCC: u8 = 0x90;
+pub const BCS: u8 = 0xB0;
+pub const BNE: u8 = 0xD0;
+pub const BEQ: u8 = 0xF0;
+
+/// Opcodes of instruction set for 6502 processor
 /// see: [6502 docs](http://www.6502.org/tutorials/6502opcodes.html)
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Opcode {
@@ -38,26 +60,26 @@ impl Opcode {
             ADC_ZERO_PAGE => Some(Opcode::Adc(value, AddressingMode::ZeroPage)),
             ADC_ZERO_PAGE_X => Some(Opcode::Adc(value, AddressingMode::ZeroPageX)),
             ADC_ABSOLUTE => Some(Opcode::Adc(value, AddressingMode::Absolute)),
-            0x7D => Some(Opcode::Adc(value, AddressingMode::AbsoluteX)),
-            0x79 => Some(Opcode::Adc(value, AddressingMode::AbsoluteY)),
-            0x61 => Some(Opcode::Adc(value, AddressingMode::IndirectX)),
-            0x71 => Some(Opcode::Adc(value, AddressingMode::IndirectY)),
-            0x29 => Some(Opcode::And(value, AddressingMode::Immediate)),
-            0x25 => Some(Opcode::And(value, AddressingMode::ZeroPage)),
-            0x35 => Some(Opcode::And(value, AddressingMode::ZeroPageX)),
-            0x2D => Some(Opcode::And(value, AddressingMode::Absolute)),
-            0x3D => Some(Opcode::And(value, AddressingMode::AbsoluteX)),
-            0x39 => Some(Opcode::And(value, AddressingMode::AbsoluteY)),
-            0x21 => Some(Opcode::And(value, AddressingMode::IndirectX)),
-            0x31 => Some(Opcode::And(value, AddressingMode::IndirectY)),
-            0x10 => Some(Opcode::Bpl(value)),
-            0x30 => Some(Opcode::Bmi(value)),
-            0x50 => Some(Opcode::Bvc(value)),
-            0x70 => Some(Opcode::Bvs(value)),
-            0x90 => Some(Opcode::Bcc(value)),
-            0xB0 => Some(Opcode::Bcs(value)),
-            0xD0 => Some(Opcode::Bne(value)),
-            0xF0 => Some(Opcode::Beq(value)),
+            ADC_ABSOLUTE_X => Some(Opcode::Adc(value, AddressingMode::AbsoluteX)),
+            ADC_ABSOLUTE_Y => Some(Opcode::Adc(value, AddressingMode::AbsoluteY)),
+            ADC_INDIRECT_X => Some(Opcode::Adc(value, AddressingMode::IndirectX)),
+            ADC_INDIRECT_Y => Some(Opcode::Adc(value, AddressingMode::IndirectY)),
+            AND_IMMEDIATE => Some(Opcode::And(value, AddressingMode::Immediate)),
+            AND_ZERO_PAGE => Some(Opcode::And(value, AddressingMode::ZeroPage)),
+            AND_ZERO_PAGE_X => Some(Opcode::And(value, AddressingMode::ZeroPageX)),
+            AND_ABSOLUTE => Some(Opcode::And(value, AddressingMode::Absolute)),
+            AND_ABSOLUTE_X => Some(Opcode::And(value, AddressingMode::AbsoluteX)),
+            AND_ABSOLUTE_Y => Some(Opcode::And(value, AddressingMode::AbsoluteY)),
+            AND_INDIRECT_X => Some(Opcode::And(value, AddressingMode::IndirectX)),
+            AND_INDIRECT_Y => Some(Opcode::And(value, AddressingMode::IndirectY)),
+            BPL => Some(Opcode::Bpl(value)),
+            BMI => Some(Opcode::Bmi(value)),
+            BVC => Some(Opcode::Bvc(value)),
+            BVS => Some(Opcode::Bvs(value)),
+            BCC => Some(Opcode::Bcc(value)),
+            BCS => Some(Opcode::Bcs(value)),
+            BNE => Some(Opcode::Bne(value)),
+            BEQ => Some(Opcode::Beq(value)),
             _ => None,
         }
     }
