42fe818375d582a8ca59385ae2b2587486975340
David Barreto <david.brto@gmail.com>
2025-02-03 13:29:28 +0000

    Add regex to identify Addressing Mode from the operand string in instruction

--- src/memory/types.rs
+++ src/memory/types.rs
@@ -1,3 +1,36 @@
+use regex::Regex;
+use lazy_static::lazy_static;
+
+const HEX_8_BIT: &str = r"(\$|0x)[0-9A-Fa-f]{2}";
+const BIN_8_BIT: &str = r"(%|0b)[01]{8}";
+const OCTAL_8_BIT:&str = r"(@|0o)[0-7]{1,3}";
+const DEC_8_BIT: &str = r"25[0-5]|2[0-4][0-9]|1?[0-9]{1,2}";
+
+const HEX_16_BIT: &str = r"(\$|0x)[0-9A-Fa-f]{4}";
+const BIN_16_BIT: &str = r"(%|0b)[01]{16}";
+const OCTAL_16_BIT: &str = r"(@|0o)[0-7]{1,5}";
+const DEC_16_BIT: &str = r"6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|[0-5]?[0-9]{1,4}";
+
+lazy_static! {
+
+    static ref NUM_8_BIT: String = format!("(?P<number>{}|{}|{}|{})", HEX_8_BIT, BIN_8_BIT, OCTAL_8_BIT, DEC_8_BIT);
+    static ref NUM_16_BIT: String = format!("(?P<number>{}|{}|{}|{})", HEX_16_BIT, BIN_16_BIT, OCTAL_16_BIT, DEC_16_BIT);
+
+    static ref ACCUMULATOR_REGEX: Regex = Regex::new(r"^A$").unwrap();
+    static ref IMPLICIT_REGEX: Regex = Regex::new(r"^$").unwrap();
+    static ref RELATIVE_REGEX: Regex = Regex::new(&format!(r"^{}$", NUM_8_BIT.as_str())).unwrap();
+    static ref IMMEDIATE_REGEX: Regex = Regex::new(&format!(r"^#{}$", NUM_8_BIT.as_str())).unwrap();
+    static ref ZERO_PAGE_REGEX: Regex = Regex::new(&format!(r"^{}$", NUM_8_BIT.as_str())).unwrap();
+    static ref ZERO_PAGE_X_REGEX: Regex = Regex::new(&format!(r"^{}\s*,\s*X$", NUM_8_BIT.as_str())).unwrap();
+    static ref ZERO_PAGE_Y_REGEX: Regex = Regex::new(&format!(r"^\${}\s*,\s*Y$", NUM_8_BIT.as_str())).unwrap();
+    static ref ABSOLUTE_REGEX: Regex = Regex::new(&format!(r"^\${}$", NUM_16_BIT.as_str())).unwrap();
+    static ref ABSOLUTE_X_REGEX: Regex = Regex::new(&format!(r"^{}\s*,\s*X$", NUM_16_BIT.as_str())).unwrap();
+    static ref ABSOLUTE_Y_REGEX: Regex = Regex::new(&format!(r"^{}\s*,\s*Y$", NUM_16_BIT.as_str())).unwrap();
+    static ref INDIRECT_REGEX: Regex = Regex::new(&format!(r"^\({}\)$", NUM_16_BIT.as_str())).unwrap();
+    static ref INDIRECT_X_REGEX: Regex = Regex::new(&format!(r"^\({}\s*,\s*X\)$", NUM_8_BIT.as_str())).unwrap();
+    static ref INDIRECT_Y_REGEX: Regex = Regex::new(&format!(r"^\({}\)\s*,\s*Y$", NUM_8_BIT.as_str())).unwrap();
+}
+
 #[derive(Debug, PartialEq)]
 pub enum MemoryError {
     AccessViolation(usize),
@@ -7,7 +40,7 @@ pub enum MemoryError {
 ///
 /// Each variant of this enum represents a specific addressing mode used by the 6502 instruction set.
 /// Addressing modes determine how the instruction will access its operands.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum AddressingMode {
     /// Absolute addressing - 16-bit address direct into the instruction
     ///
@@ -100,9 +133,7 @@ pub enum AddressingMode {
     /// The same as Zero Page, but the Y register is added to the address.
     ///
     /// Example: `LDA $20,Y` will load the value at zero-page address `$20 + Y` into the accumulator.
-    ZeroPageY,
-    /// No addressing mode
-    None,
+    ZeroPageY
 }
 
 impl AddressingMode {
@@ -120,8 +151,25 @@ impl AddressingMode {
             AddressingMode::IndirectX => 2,
             AddressingMode::IndirectY => 2,
             AddressingMode::Accumulator => 1,
-            AddressingMode::Relative => 2,
-            AddressingMode::None => 0
+            AddressingMode::Relative => 2
+        }
+    }
+
+    pub fn regex(&self) -> &'static Regex {
+        match self {
+            AddressingMode::Implicit => &IMPLICIT_REGEX,
+            AddressingMode::Immediate => &IMMEDIATE_REGEX,
+            AddressingMode::ZeroPage => &ZERO_PAGE_REGEX,
+            AddressingMode::ZeroPageX => &ZERO_PAGE_X_REGEX,
+            AddressingMode::ZeroPageY => &ZERO_PAGE_Y_REGEX,
+            AddressingMode::Absolute => &ABSOLUTE_REGEX,
+            AddressingMode::AbsoluteX => &ABSOLUTE_X_REGEX,
+            AddressingMode::AbsoluteY => &ABSOLUTE_Y_REGEX,
+            AddressingMode::Indirect => &INDIRECT_REGEX,
+            AddressingMode::IndirectX => &INDIRECT_X_REGEX,
+            AddressingMode::IndirectY => &INDIRECT_Y_REGEX,
+            AddressingMode::Accumulator => &ACCUMULATOR_REGEX,
+            AddressingMode::Relative => &RELATIVE_REGEX
         }
     }
 }
